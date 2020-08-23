3f2d1bd4edbe3e8cd5f9f57a136bdaaf4a8c1d20
l-winston <winwinstonliu@gmail.com>
2020-08-23 02:58:07 -0700

    renamed

---
+++ compile.py
@@ -0,0 +1,43 @@
+import os
+
+startup = "import numpy as np\nMEMORY_LENGTH = 30000\nmemory = np.zeros((MEMORY_LENGTH), dtype=np.int8)\nindex = 0\n"
+
+def main():
+    file_in = open("in.txt", "r")
+
+    file_out = open("out.py", "w")
+    file_out.truncate(0)
+    file_out.write(startup)
+
+    tabs = 0
+    for line_num, line in enumerate(file_in):
+        for i in range(len(line)):
+            c = line[i]
+
+            prefix = tabs * "\t"
+
+            if c == ">":
+                file_out.write(prefix + "index+=1\n")
+            elif c == "<":
+                file_out.write(prefix + "index-=1\n")
+            elif c == "+":
+                file_out.write(prefix + "memory[index]+=1\n")
+            elif c == "-":
+                file_out.write(prefix + "memory[index]-=1\n")
+            elif c == ".":
+                file_out.write(prefix + 'print(chr(memory[index]), end="")\n')
+            elif c == ",":
+                file_out.write(prefix + "memory[index]=int(input())\n")
+            elif c == "[":
+                file_out.write(prefix + "while(memory[index]):\n")
+                tabs += 1
+            elif c == "]":
+                tabs -= 1
+
+    file_out.close()
+    print("Executing file_out.py...")
+    os.system("python file_out.py")
+
+
+if __name__ == "__main__":
+    main()
